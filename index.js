257551331e4e254250bc134c64f1b3a4546d81a4
Felix Itzenplitz <felix@stkn.org>
2016-06-30 12:07:43 +0200

    fix

--- index.js
+++ index.js
@@ -4,28 +4,34 @@ process.title = 'pcp'
 var path = require('path');
 
 var argv = require('yargs')
-        .usage('Usage: $0 -p [str] file.json')
-        .demand(['p', 1])
-        .argv;
+    .usage('Usage: $0 -p [str] file.json')
+    .demand(['p', 1])
+    .argv;
 var transformer = require('postman-collection-transformer');
 var pad = require('left-pad');
 var jsonfile = require('jsonfile');
 
+var prefix = '#' + argv.p;
+var collection = require(path.resolve('.', argv._[0]));
+
 function addNum(str, num) {
     // remove old prefix
     str = str.replace(/^#[A-Z]+[0-9]+\s-\s/, '');
+
     return prefix + pad(num, 2, 0) + '0' + ' - ' + str;
 }
 
-function addNumPoint(str, num, point) {
+function addNumNum(str, num, num2) {
     // remove old prefix
     str = str.replace(/^#[A-Z]+[0-9]+\.[0-9]+\s-\s/, '');
-    return prefix + pad(num, 2, 0) + '0' + '.' + pad(point, 2, 0) + '0' + ' - ' + str;
+
+    return prefix + pad(num, 2, 0) + '0' + '.' + pad(num2, 2, 0) + '0' + ' - ' + str;
 }
 
 function prefixIt(collection) {
     // remove old prefix
     collection.info.name = collection.info.name.replace(/^#[A-Z]+\s-\s/, '');
+
     collection.info.name = prefix + ' - ' + collection.info.name
     console.log(collection.info.name);
 
@@ -36,7 +42,7 @@ function prefixIt(collection) {
         if (item.item.length) {
             var j = 1;
             for (let item2 of item.item) {
-                item2.name = addNumPoint(item2.name, i, j++);
+                item2.name = addNumNum(item2.name, i, j++);
                 console.log(item2.name);
             }
         }
@@ -44,35 +50,23 @@ function prefixIt(collection) {
     }
 }
 
-var prefix = '#' + argv.p;
-var collection = require(path.resolve('.', argv._[0]));
-
-var options = {
-    inputVersion: '1.0.0',
-    outputVersion: '2.0.0'
-};
-
 // v1 -> v2
-transformer.convert(collection, options, function (error, result) {
+transformer.convert(collection, { inputVersion: '1.0.0', outputVersion: '2.0.0' }, function (error, result) {
     if (error) {
         return console.error(error);
     }
 
+    // do it
     prefixIt(result);
 
-    var options = {
-        inputVersion: '2.0.0',
-        outputVersion: '1.0.0'
-    };
-
     // v2 -> v1
-    transformer.convert(result, options, function (error2, result2) {
+    transformer.convert(result, { inputVersion: '2.0.0', outputVersion: '1.0.0' }, function (error2, result2) {
         if (error2) {
             return console.error(error2);
         }
 
         // write to disk
-        jsonfile.writeFile(path.resolve('.', argv._[0]), result2, {spaces: 2}, function (err) {
+        jsonfile.writeFile(path.resolve('.', argv._[0]), result2, { spaces: 2 }, function (err) {
             if (err) {
                 console.error(err)
             }
