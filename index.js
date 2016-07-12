321509744087b6c77ae53b2a083a8454f0cb246c
Felix Itzenplitz <cebor@users.noreply.github.com>
2016-07-12 11:43:33 +0200

    Add recursive behaviour

--- index.js
+++ index.js
@@ -1,75 +1,89 @@
 #!/usr/bin/env node
 process.title = 'pcp'
 
-var path = require('path');
-
 var argv = require('yargs')
-    .usage('Usage: $0 -p [str] file.json')
+    .usage('Usage: pcp -p [str] file.json')
     .demand(['p', 1])
+    .string('p')
+    .normalize(1)
     .argv;
-var transformer = require('postman-collection-transformer');
+
 var pad = require('left-pad');
-var jsonfile = require('jsonfile');
-
 var prefix = '#' + argv.p;
-var collection = require(path.resolve('.', argv._[0]));
 
-function addNum(str, num) {
-    // remove old prefix
-    str = str.replace(/^#[A-Z]+[0-9]+\s-\s/, '');
-
-    return prefix + pad(num, 2, 0) + '0' + ' - ' + str;
-}
-
-function addNumNum(str, num, num2) {
-    // remove old prefix
-    str = str.replace(/^#[A-Z]+[0-9]+\.[0-9]+\s-\s/, '');
-
-    return prefix + pad(num, 2, 0) + '0' + '.' + pad(num2, 2, 0) + '0' + ' - ' + str;
-}
-
-function prefixIt(collection) {
-    // remove old prefix
-    collection.info.name = collection.info.name.replace(/^#[A-Z]+\s-\s/, '');
-
-    collection.info.name = prefix + ' - ' + collection.info.name
-    console.log(collection.info.name);
-
-    var i = 1;
-    for (let item of collection.item) {
-        item.name = addNum(item.name, i);
-        console.log(item.name);
-        if (item.item.length) {
-            var j = 1;
-            for (let item2 of item.item) {
-                item2.name = addNumNum(item2.name, i, j++);
-                console.log(item2.name);
-            }
-        }
-        i++;
-    }
-}
-
-// v1 -> v2
-transformer.convert(collection, { inputVersion: '1.0.0', outputVersion: '2.0.0' }, function (error, result) {
-    if (error) {
-        return console.error(error);
+function prefixIt(collection, prevCounters = []) {
+    function addNum(str, num, pre) {
+        // remove old prefix
+        str = str.replace(/^#[a-zA-Z0-9]+(\.[0-9]+)*\s-\s/, '');
+        
+        // add prefix
+        var ret = prefix;
+        prevCounters.forEach(function (index, i) {
+            ret = ret + (i > 0 ? '.' : '') + pad(index, 2, 0) + '0';
+        });
+        ret = ret + (prevCounters.length ? '.' : '') + pad(num, 2, 0) + '0';
+ 
+        return ret + ' - ' + str;
     }
 
-    // do it
-    prefixIt(result);
+    // first level
+    if (collection.info) {
+        // remove old prefix
+        collection.info.name = collection.info.name.replace(/^#[A-Z]+\s-\s/, '');
+        collection.info.name = prefix + ' - ' + collection.info.name
+        console.log(collection.info.name);
 
-    // v2 -> v1
-    transformer.convert(result, { inputVersion: '2.0.0', outputVersion: '1.0.0' }, function (error2, result2) {
-        if (error2) {
-            return console.error(error2);
+        // prefix childs
+        if (collection.item && collection.item.length) {
+            prefixIt(collection.item);
         }
+    // deeper levels
+    } else {
+        collection.forEach(function (item, i) {
+            // start with 1, not with 0
+            var counter = i + 1;
+            item.name = addNum(item.name, counter);
+            console.log(item.name);
 
-        // write to disk
-        jsonfile.writeFile(path.resolve('.', argv._[0]), result2, { spaces: '\t' }, function (err) {
-            if (err) {
-                console.error(err)
+            // prefix childChilds
+            if (item.item && item.item.length) {
+                prefixIt(item.item, [...prevCounters, counter]);
             }
         });
+    }
+}
+
+function adaptCollection(filePath, adapter) {
+    var path = require('path');
+
+    var transformer = require('postman-collection-transformer');
+    var jsonfile = require('jsonfile');
+
+    var collection = require(path.resolve(filePath));
+
+    // v1 -> v2
+    transformer.convert(collection, { inputVersion: '1.0.0', outputVersion: '2.0.0' }, function (error, result) {
+        if (error) {
+            return console.error(error);
+        }
+
+        // do it
+        adapter(result);
+
+        // v2 -> v1
+        transformer.convert(result, { inputVersion: '2.0.0', outputVersion: '1.0.0' }, function (error, result) {
+            if (error) {
+                return console.error(error);
+            }
+
+            // write to disk
+            jsonfile.writeFile(path.resolve(filePath), result, { spaces: '\t' }, function (error) {
+                if (error) {
+                    console.error(error)
+                }
+            });
+        });
     });
-});
+}
+
+adaptCollection(argv._[0], prefixIt);
